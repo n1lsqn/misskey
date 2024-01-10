f62092a47ad1b411a598d1d28fe92322923910f0
n1lsqn <owner@n1l.dev>
2024-01-11 03:50:55 +0900

    chore: version修正

--- update.sh
+++ update.sh
@@ -2,6 +2,6 @@
 git pull
 git submodule update --init
 NODE_ENV=production pnpm install --frozen-lockfile
-NODE_ENV=production NODE_OPTIONS='--max-old-space-size=1024' pnpm run build
+NODE_ENV=production NODE_OPTIONS='--max-old-space-size=512' pnpm run build
 pnpm run migrate
 sudo service misskey restart
