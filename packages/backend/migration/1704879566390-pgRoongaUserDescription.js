3650092ca6f42f75db9ef5ebaecd11e94adc7959
n1lsqn <owner@n1l.dev>
2024-01-11 23:32:34 +0900

    fix: migration

--- packages/backend/migration/1704879566390-pgRoongaUserDescription.js
+++ packages/backend/migration/1704879566390-pgRoongaUserDescription.js
@@ -2,7 +2,7 @@ export class PgRoongaUserDescription1704879566390 {
     name = 'PgRoongaUserDescription1704879566390'
 
 		async up(queryRunner) {
-			await queryRunner.query(`CREATE INDEX "IDX_PGROONGA_USER_DESCRIPTION" ON "user" USING pgroonga("description");`)
+			await queryRunner.query(`CREATE INDEX "IDX_PGROONGA_USER_DESCRIPTION" ON "user" USING pgroonga("user_profile");`)
     }
 
 		async down(queryRunner) {
